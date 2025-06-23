4aa59da913b07d7898bc7792cdf54e2da1e84b0b
Mehvi <mehvishgully@gmail.com>
2025-06-23 12:25:08 +0500

    create file

---
+++ Bitwise_and_Logical_Operations/perm.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdint.h>
+#include<stdbool.h>
+#define PERM_READ  (1 << 0)
+#define PERM_WRITE (1 << 1)
+#define PERM_EXEC  (1 << 2)
+bool has_permission(uint8_t user_perm, uint8_t required_perm){
+
+}
+int main(){
+    
+}
