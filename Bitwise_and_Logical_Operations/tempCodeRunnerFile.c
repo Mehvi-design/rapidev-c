4db597dde564d50e7c21aba07beec3fe9c26b4e3
Mehvi <mehvishgully@gmail.com>
2025-06-23 16:07:07 +0500

    macros edited

--- Bitwise_and_Logical_Operations/tempCodeRunnerFile.c
+++ Bitwise_and_Logical_Operations/tempCodeRunnerFile.c
@@ -2,22 +2,29 @@
 #include<stdint.h>
 
 typedef struct {
-uint8_t direction;
-uint8_t output;
-uint8_t input;
+uint8_t direction;//output 1,input 0
+uint8_t output;//pin output values
+uint8_t input;//pin input values
 } GPIO_Port;
 //macros
+//read input and output values
 #define GPIO_READ_INPUTPIN(PORT,pin)
 #define GPIO_READ_OUTPUTPIN(PORT,pin)
-#define GPIO_WRITE_PINHIGH(PORT,mask)
-#define GPIO_WRITE_PINLOW(PORT,mask)
+//write specific output pins high or low
+#define GPIO_WRITE_OUTPINHIGH(PORT,mask)
+#define GPIO_WRITE_OUTINLOW(PORT,mask)
+//write specific input pins low or high
+#define GPIO_WRITE_INPINLOW(PORT,mask)
+#define GPIO_WRITE_INPINHIGH(PORT,mask)
+//Set directio of specific pins input or high
 #define GPIO_SET_PINOUTPUT(PORT,mask)
-#define GPIO_SET_PININPUT(PORT,mask)
-#define GPIO_TOGGLE_PIN(PORT,mask)(PORT.output=~(PORT.input|(1<<mask)))
-
+#define GPIO_SET_PININPUT(PORT,mask)(PORT.input=)
+//toggle specific bit or toggle all bit
+#define GPIO_TOGGLE_PIN(PORT,mask)((PORT).output^=(mask))
+#define GPIO_TOGGLE_AllPIN(PORT,mask)((PORT).output^=(1<<mask))
 int main(){
     GPIO_Port portA={0};
-    printf("%d",GPIO_TOGGLE_PIN(portA,0));
+    printf("%d",GPIO_TOGGLE_PIN(portA,1));
 }
 
 
