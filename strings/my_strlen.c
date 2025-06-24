7a7c35508667bf71322d179c93bb5e16fff2c6a9
Mehvi <mehvishgully@gmail.com>
2025-06-24 09:22:07 +0500

    my string changes

---
+++ strings/my_strlen.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include <stddef.h> // For size_t
+size_t my_strlen(const char *str){
+
+   size_t length=0;
+   while(*str!='\0'){
+    length++;
+    str++;
+
+   }
+   return length;
+}
+    
+int main(){
+        char str[]= "hello";
+    printf("%d",my_strlen(str));
+}
